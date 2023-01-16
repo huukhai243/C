e1f75c6c9513183e4bf7a2919ae917983c5f287c
huukhai243 <91890454+huukhai243@users.noreply.github.com>
2023-01-16 23:53:26 +0700

    Add files via upload

---
+++ Untitled68c.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main() 
+{
+float A,B,C;
+ 
+printf("\nEnter side A ") ;
+scanf ("%f",&A) ;
+printf("\nEnter side B ") ;
+scanf ("%f ",&B) ;
+C=sqrt( A*A + B*B ); 
+printf("side C : %f ", C) ;
+return 0;
+}
